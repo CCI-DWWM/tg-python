63c04dfe36ce21bacf3300655ff2e66bc58f48d5
ThibautG <6084433+ThibautG@users.noreply.github.com>
2025-06-26 14:40:33 +0200

    test

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+"""
+
 # conditions inline
 x = 10
 s = "Even" if x % 2 == 0 else "Odd"
@@ -21,3 +23,22 @@ for x in l2:
 l3 = range(5,10)
 for x in l3:
     print(f"- {x=}")
+
+
+# for ... else
+l4 = range(2, 101, 2)
+for x in l4:
+    if x == 51:
+        break
+    print(f"- {x=}")
+else:
+    print('cas ELSE')
+    
+"""
+prenoms = ["Pierre", "Patrick", "Jean", "Marc"]
+for prenom in prenoms:
+    if prenom == "Patrick":
+        print("Patrick a été trouvé !")
+        break
+else:
+    print("Patrick est introuvable...")
