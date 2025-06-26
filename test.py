e3b9771ffeeb4edcbdf541df5bece4bc94849a08
ThibautG <6084433+ThibautG@users.noreply.github.com>
2025-06-26 11:58:45 +0200

    test

--- test.py
+++ test.py
@@ -1,6 +1,23 @@
-surname = input('Entrez votre prénom : ')
-name = (input('Entrez votre nom : '))
-dateOfBirth = int(input('Entrez votre année de naissance : '))
-age = 2025 - dateOfBirth
+# conditions inline
+x = 10
+s = "Even" if x % 2 == 0 else "Odd"
+print(s)
 
-print(f"Bonjour {surname.upper()} {name.upper()}, vous avez {age} ans")
+# boucles while
+i = 0
+while i < 5:
+    print(i)
+    i = i + 1
+
+# for ... in
+l1 = [1, 2, 4, 8]
+for x in l1:
+    print(f"- {x=}")
+
+l2 = "Hello World !"
+for x in l2:
+    print(f"- {x=}")
+
+l3 = range(5,10)
+for x in l3:
+    print(f"- {x=}")
