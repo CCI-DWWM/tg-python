ed6b2a9192a549b8da6cefe8d220504a31120c0d
ThibautG <6084433+ThibautG@users.noreply.github.com>
2025-06-27 14:11:54 +0200

    dixième exercice V3

--- exo10.py
+++ exo10.py
@@ -4,14 +4,21 @@ Créer une fonction qui permette d'afficher lun nom encadré :
 │ Emmanuel │
 └──────────┘
 """
+import sys
 from colorama import Fore, Back, Style
 
-def encadre(name):
-    nb_chars = len(name) + 2
-    line = "─" * nb_chars
-    red_name = Fore.RED + name + Style.RESET_ALL
+def encadre(name, color):
+    x = '─' * len(name)
+    couleur = getattr(Fore, color.upper())
+    print(f"""
+┌─{x}─┐
+│ {couleur}{name}{Style.RESET_ALL} │
+└─{x}─┘""")
 
-    print(f"┌{line}┐\n│ {red_name} │\n└{line}┘")
+if __name__ == "__main__":
+    print(f"=> {sys.argv[1]}")
 
-encadre("Thibaut")
-encadre("Coucou")
+    encadre(sys.argv[1], sys.argv[2])
+
+# pour utiliser la fonction :
+# CMD ==> py exo10.py Nom couleur
